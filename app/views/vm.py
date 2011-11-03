9263a331b6771d79140852243afc3d6e2ffd0246
Gosuke Miyashita <gosukenator@gmail.com>
2011-11-03 09:55:35 -0400

    add console view

--- app/views/vm.py
+++ app/views/vm.py
@@ -3,7 +3,14 @@ from flask import Blueprint, render_template
 app = Blueprint('vm', __name__)
 import maglica.vm
 
-@app.route('/')
+@app.route('')
 def list():
-    virtual_machines = maglica.vm.list()
-    return render_template('vm/list.html', virtual_machines=virtual_machines)
+    vms = maglica.vm.list()
+    return render_template('vm/list.html', vms=vms)
+
+@app.route('/console/<name>')
+def console(name):
+    info = maglica.vm.info({"name": name})
+    return render_template('vm/console.html', info=info)
+
+
